b13ea9b0088f1b5e03cc64c877233e358b918fb1
AlbertAdekanye <adekanyealbert22@gmail.com>
2024-08-09 00:53:37 +0100

    create pages and routes and header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,20 @@ import About from './pages/About';
 import Dashboard from './pages/Dashboard';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import Projects from './pages/Projects';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/projects" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects/>} />
       </Routes>
     </BrowserRouter>
   )
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import { Navbar } from 'flowbite-react';
+
+export default function Header() {
+  return (
+    <Navbar className='border-b-2 text-blue-500 bold uppercase cursor-pointer text-2xl'>
+      Exam
+    </Navbar>
+  )
+}
