0ec702bbaba00d403b4d2b4f7d3f21a37e21b445
AlbertAdekanye <adekanyealbert22@gmail.com>
2024-08-12 22:03:50 +0100

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,11 +7,15 @@ import { FaMoon } from 'react-icons/fa';
 export default function Header() {
   const path= useLocation().pathname;
   return (
+
+    // Navbar from flowbite-react
     <Navbar className='border-b-2'>
+      {/* link from react router-dom */}
       <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Albert's</span>
         Blog
       </Link>
+      {/* search input */}
       <form>
         <TextInput 
           type='text'
@@ -28,13 +32,14 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue' pill>
+          <Button gradientDuoTone='purpleToBlue' outline>
             Sign In
           </Button>
         </Link>
         <Navbar.Toggle />
       </div>
-
+      
+      {/* Nav link */}
       <Navbar.Collapse>
         <Navbar.Link active={path === "/"} as={'div'}>
           <Link to='/'>
