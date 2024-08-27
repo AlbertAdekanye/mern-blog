44f5e5533872625acec81ab265e027d8eae8d2d0
AlbertAdekanye <adekanyealbert22@gmail.com>
2024-08-28 00:13:09 +0100

    #

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,21 +1,52 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { Alert, Button, Checkbox, Label, Spinner, TextInput } from "flowbite-react";
 import { Link, useNavigate } from "react-router-dom";
+import { HiMail } from "react-icons/hi";
 import logo from '../assets/logo.png';
 import { useState } from "react";
+// import validator from 'validator';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: '',
+  });
+  
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+    if (e.target.id === 'remember') {
+      const {username, password } = credentials;
+      if (e.target.checked) {
+        localStorage.setItem('credentials', JSON.stringify({ username, password}));
+      } else {
+        localStorage.removeItem('credentials');
+      }
+    }
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const storedCredentials = localStorage.getItem('credentials');
+    if (storedCredentials) {
+      const { username, password } = JSON.parse(storedCredentials);
+      setCredentials({ username, password });
+    }
+    //  const form = e.target;
+    //  form.submit();
     if (!formData.username || !formData.email || !formData.password) {
       return setErrorMessage('Please fill in all fields');
+    }
+    // validation code
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/;
+    if (!emailRegex.test(formData.email)) {
+      return setErrorMessage('Invalid email format');
+    }
+    if (!passwordRegex.test(formData.password)) {
+      return setErrorMessage('Password must be at least 8 characters, including letters and numbers');
     }
     try {
       setLoading(true);
@@ -64,6 +95,7 @@ export default function SignUp() {
               type='text'
               placeholder='username'
               id='username'
+              addon="@"
               className='w-full'
               onChange={handleChange}
             />
@@ -72,7 +104,8 @@ export default function SignUp() {
             <Label value='Your email:'/>
             <TextInput 
               type='email'
-              placeholder='name@gmail.com'
+              icon={HiMail}
+              placeholder='@gmail.com'
               id='email'
               className='w-full'
               onChange={handleChange}
@@ -87,6 +120,10 @@ export default function SignUp() {
               className='w-full'
               onChange={handleChange}
             />
+            <div className="flex items-center gap-2 mt-2">
+              <Checkbox id="remember" />
+              <Label htmlFor="remember">Remember me</Label>
+            </div>
           </div>
             <Button gradientDuoTone='purpleToBlue' type='submit' disable={loading}>
               {loading ? (
