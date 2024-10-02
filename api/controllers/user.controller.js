9388bb95afd4b66833198124d2dbeba68cc3c988
AlbertAdekanye <adekanyealbert22@gmail.com>
2024-10-02 14:05:04 +0100

    complete profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -30,6 +30,7 @@ export const updateUser = async (req, res, next) => {
     if(!req.body.username.match(/^[0-9a-zA-Z]+$/)){
       return next(errorHandler(400, 'Username must contain only letters and numbers'));
     }
+  }
     try {
       const updatedUser = await User.findByIdAndUpdate(req.params.userId, {
         $set: {
@@ -44,5 +45,4 @@ export const updateUser = async (req, res, next) => {
     } catch (error) {
       next(error);
     }
-  }
 };
